78e3b3ef89aeec60d47f3fff0ec712785c80caa1
lingamurthi <lingamurthy5364@gmail.com>
2023-01-18 11:41:48 +0530

    added mongodb

--- app.js
+++ app.js
@@ -44,3 +44,17 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`app is running at ${port}`);
 });
+
+
+
+
+
+
+
+
+// Making Build Folder as Public 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
